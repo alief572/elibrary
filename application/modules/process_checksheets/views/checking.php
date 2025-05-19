4a72234ebd802d83528ee77aa8abf5d158b5de48
alief572 <146901652+alief572@users.noreply.github.com>
2025-05-19 10:45:33 +0700

    awd

--- application/modules/process_checksheets/views/checking.php
+++ application/modules/process_checksheets/views/checking.php
@@ -63,8 +63,7 @@
 											if (
 												($fChecking[$data->frequency_checking] == 'Daily' && $i == date('d')) ||
 												($fChecking[$data->frequency_checking] == 'Weekly' && $i == $weekOfMonth) ||
-												($fChecking[$data->frequency_checking] == 'Monthly' && $i == date('m')) ||
-												($fChecking[$data->frequency_checking] == 'Daily' && $i == '13')
+												($fChecking[$data->frequency_checking] == 'Monthly' && $i == date('m'))
 											) {
 
 
@@ -73,7 +72,7 @@
 												if (date('w', strtotime($tanggalkolom)) % 6 == 0) $weekend = "table-danger";
 												if ($data->frequency_checking != 1) $weekend = "";
 										?>
-												<th class="text-center <?= $weekend ?> <?= $i < (date('d')) ? 'ds-none' : ''; ?>  <?= ($weekOfMonth) && ($weekOfMonth == $i) ? 'bg-light-warning' : (($exec == 3 && ($i == date('d') || $i == '13')) ? 'bg-light-warning' : (($exec == 5 && $i == date('m')) ? 'bg-light-warning' : '')); ?>"><?= ($exec != 5) ? $name_col . " " . $i : $name_col[$i]; ?></th>
+												<th class="text-center <?= $weekend ?> <?= $i < (date('d')) ? 'ds-none' : ''; ?>  <?= ($weekOfMonth) && ($weekOfMonth == $i) ? 'bg-light-warning' : (($exec == 3 && $i == date('d')) ? 'bg-light-warning' : (($exec == 5 && $i == date('m')) ? 'bg-light-warning' : '')); ?>"><?= ($exec != 5) ? $name_col . " " . $i : $name_col[$i]; ?></th>
 											<?php } ?>
 										<?php } ?>
 									</tr>
@@ -99,8 +98,8 @@
 												if (
 													($fChecking[$data->frequency_checking] == 'Daily' && $i == date('d')) ||
 													($fChecking[$data->frequency_checking] == 'Weekly' && $i == $weekOfMonth) ||
-													($fChecking[$data->frequency_checking] == 'Monthly' && $i == date('m')) ||
-													($fChecking[$data->frequency_checking] == 'Daily' && $i == 13)) {
+													($fChecking[$data->frequency_checking] == 'Monthly' && $i == date('m'))
+												) {
 
 													$weekend = "";
 													$tanggalkolom = date("Y-m", strtotime($data->periode)) . "-" . $i;
@@ -112,14 +111,14 @@
 													<?php $NBukti = "bukti_" . $i; ?>
 													<input type="hidden" name="detail[<?= $n . "_" . $i; ?>][id]" value="<?= $it->id; ?>" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d')) ? 'disabled' : (($exec == 5) && ($i != (date('m'))) ? 'disabled' : '')); ?>>
 													<input type="hidden" name="detail[<?= $n . "_" . $i; ?>][field]" value="<?= $i; ?>" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d')) ? 'disabled' : (($exec == 5) && ($i != (date('m'))) ? 'disabled' : '')); ?>>
-													<td class="<?= $weekend ?> <?= ($weekOfMonth) && ($weekOfMonth == $i) ? 'bg-light-warning' : (($exec == 3 && ($i == date('d') || $i == '13')) ? 'bg-light-warning' : (($exec == 5 && $i == date('m')) ? 'bg-light-warning' : '')); ?>">
+													<td class="<?= $weekend ?> <?= ($weekOfMonth) && ($weekOfMonth == $i) ? 'bg-light-warning' : (($exec == 3 && $i == date('d')) ? 'bg-light-warning' : (($exec == 5 && $i == date('m')) ? 'bg-light-warning' : '')); ?>">
 														<br>
 														<?php if ($it->check_type == 'boolean') : ?>
 															<div class="" id="r_<?= $n . '_c_' . $i; ?>">
 																<div class="d-flex justify-content-start align-items-center gap-4">
 																	<div class="form-check form-check-custom form-check-solid mr-10">
 																		<label class="form-check-label font-weight-bolder text-dark">
-																			<input class="form-check-input yes required" type="radio" value="yes" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d') || $i !== '13') ? '' : (($exec == 5) && ($i != date('m')) ? 'disabled' : '')); ?> name="detail[<?= $n . "_" . $i; ?>][n<?= $i; ?>]" data-row="<?= $n . $i; ?>" id="boolean_<?= $i . $n; ?>" <?= ($it->$nn == 'yes') ? 'checked' : ''; ?>>
+																			<input class="form-check-input yes required" type="radio" value="yes" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d')) ? 'disabled' : (($exec == 5) && ($i != (date('m'))) ? 'disabled' : '')); ?> name="detail[<?= $n . "_" . $i; ?>][n<?= $i; ?>]" data-row="<?= $n . $i; ?>" id="boolean_<?= $i . $n; ?>" <?= ($it->$nn == 'yes') ? 'checked' : ''; ?>>
 																			Yes
 																			<span class="invalid-feedback font-weight-normal">
 																				<i class="text-danger fa fa-exclamation-circle"></i>
@@ -128,7 +127,7 @@
 																	</div>
 																	<div class="form-check form-check-custom form-check-danger form-check-solid mr-10">
 																		<label class="form-check-label font-weight-bolder text">
-																			<input class="form-check-input no required" type="radio" value="no" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d')) ? '' : (($exec == 5) && ($i != (date('m'))) ? 'disabled' : '')); ?> name="detail[<?= $n . "_" . $i; ?>][n<?= $i; ?>]" data-row="<?= $n . $i; ?>" id="boolean_<?= $i . $n; ?>" <?= ($it->$nn == 'no') ? 'checked' : ''; ?>>
+																			<input class="form-check-input no required" type="radio" value="no" <?= ($weekOfMonth) ? (($weekOfMonth != $i) ? 'disabled' : '') : ($exec == 3 && ($i != date('d')) ? 'disabled' : (($exec == 5) && ($i != (date('m'))) ? 'disabled' : '')); ?> name="detail[<?= $n . "_" . $i; ?>][n<?= $i; ?>]" data-row="<?= $n . $i; ?>" id="boolean_<?= $i . $n; ?>" <?= ($it->$nn == 'no') ? 'checked' : ''; ?>>
 																			No
 																			<span class="invalid-feedback font-weight-normal">
 																				<i class="text-danger fa fa-exclamation-circle fa-md"></i>
